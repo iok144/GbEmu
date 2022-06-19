6b03553a0383e3ab3a6bb0d974f769050105be8b
iok144 <iok119497@gmail.com>
2022-06-19 18:39:02 +0500

    Deleted unnecessary code

--- GbEmu/Program.cs
+++ GbEmu/Program.cs
@@ -6,9 +6,6 @@ namespace GbEmu
     {
         static void Main(string[] args)
         {
-            var a = 0x0000 - 1;
-            Console.WriteLine(Convert.ToString((UInt16)a, toBase: 16));
-
             var emulator = new Emulator();
 
             emulator.RunEmu(args);
